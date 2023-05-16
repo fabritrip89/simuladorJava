cf9a2449bafa4234d84cda09a3ad92c56ca6dab7
fabricio tripicchio <fabriciot89@gmail.com>
2023-05-16 20:11:44 -0300

    3erEntrega16-05

--- js/welcomePage.js
+++ js/welcomePage.js
@@ -1,15 +1,12 @@
-let ingreseSuEdad = parseInt(prompt("Ingrese su edad") );
+
+let ingreseSuEdad = parseInt (prompt("Enter your age") );
 
 if (ingreseSuEdad >=18) {
-    alert ("Welcome");
-    
-    
-} else if (ingreseSuEdad<=18){
-    alert ("No puede comprar en este sitio")
+    swal.fire ({ title:  '<span class= "tituloToThemoon"/> Welcome!', footer: '<span> Sincerely paddle to the moon', })
+        
+}else {swal.fire ({ title: "You can't buy on this site", text: "You will be redirected to google",icon: 'error', footer: '<a href="..."> Terms and Conditions</a>'})
     setTimeout(function() {
         window.location.href = 'https://www.google.com/'; 
-    },200  )
-    
-    
-}
+    },5000  )
 
+}
